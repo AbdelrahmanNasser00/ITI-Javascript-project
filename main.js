439ea2f1427a073fa3fc30d4780ab889a948a496
Ardina20 <ardinaasad43@gmail.com>
2024-12-23 22:08:26 +0200

    added the main structure of the home page

---
+++ main.js
@@ -0,0 +1,23 @@
+/***************************************HOME PAGE*****************************************/
+var ul=[
+    {
+        name:"Home",
+        link:"#"
+    },
+    {
+        name:"About",
+        link:"#"
+    },
+    {
+        name:"contact us",
+        link:"#"
+    },
+    {
+        name:"Offers",
+        link:"#"
+    },
+    {
+        name:"new",
+        link:"#"
+    }
+]
