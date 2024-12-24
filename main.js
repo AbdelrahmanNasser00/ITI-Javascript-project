779634881af902ac95ad215878b4a4261f481d3f
Ardina20 <ardinaasad43@gmail.com>
2024-12-24 22:01:56 +0200

    added the main structure of the home page

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 /***************************************HOME PAGE*****************************************/
+
 var ul = [
   {
     name: "Home",
@@ -22,6 +23,34 @@ var ul = [
   },
 ];
 
+var list=document.getElementById("navLinks");
+var cards=document.getElementById("cards")
+
+
+function navLinks(){
+  for(var i=0;i<ul.length;i++)
+  {
+    var listItem=document.createElement("li");
+    listItem.innerHTML=`<a href=${ul[i].link} class="navIl">${ul[i].name}</a>`;
+    list.append(listItem);
+  };
+};
+
+navLinks()
+
+list.style.cssText=
+  `
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap:70px;
+    list-style:none ;
+
+  `;
+
+
+
+/*********************************************************************************************** */
 // logout button
 function logout() {
   sessionStorage.removeItem("currentUser");
